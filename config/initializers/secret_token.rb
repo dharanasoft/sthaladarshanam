88b1b769d295657f82cf6ced2a8acd340723c0b7
vagmi <vagmi.mudumbai@gmail.com>
2011-05-29 21:56:16 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sthaladharshanam::Application.config.secret_token = 'aee79c199e995365237403cfdc17e7190b010999f14c5cbc68fd2bfaf82d7ae521e2b0810a60d5b8f124b12fb8d7fedcd8dd9e6ef3c12dc0c6ba85d0698f36a1'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Sthaladharshanam::Application.routes.draw do
+  root :to=>"welcome#index"
+end
